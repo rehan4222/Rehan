f4d63a8582c28c5bb5c3c1a418ff599afd72eda4
rehan4222 <mrh4222@outlook.com>
2016-05-27 15:08:15 +0500

    Add files via upload

---
+++ version 2/WindowsFormsApplication2/Rehan/gpaCal/gpaCal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gpaCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="0";
+            int age=0;
+            char sec='0';
+            double salary=0;
+            double cgpa=0;
+            info(ref name, ref age, ref sec, ref salary, ref cgpa);
+            Console.WriteLine("uper wali values {0},{1},{2}",name,age,sec);
+                   
+        }
+        public static void info(ref string name,ref int age,ref char sec,ref double salary, ref double cgpa)
+        {
+             //String name;
+            //int age;
+            //char sec;
+           // Double salary;
+           // Double cgpa;
+            Console.WriteLine("Enter your Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your section:");
+            sec=Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("enter your salary serious wali");
+            salary=Convert.ToDouble((Console.ReadLine()));
+            Console.WriteLine("enter your cgpa");
+            cgpa=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0},{1},{2}",name,age,sec);
+        }
+    }
+}
---
+++ version 2/WindowsFormsApplication2/Rehan/practise1/practise1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int number = 3;
+
+            Console.WriteLine(sumValue(a, number));
+            Console.WriteLine(Forward(a, number));
+        }
+        public static int sumValue(int[] arr, int value)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    for (int j = i; j >= 0; j--)
+                    {
+                        sum += arr[j];
+                    }
+                }
+            }
+            return sum;
+        }
+        public static int Forward(int[] arra,int values)
+        {
+            int sumF = 0;
+            for (int i = 0; i < arra.Length; i++)
+            {
+                if (arra[i]==values)
+                {
+                    for (int j = i; j <arra.Length ; j++)
+                    {
+                        sumF += arra[j];
+                    }
+                }
+            }
+                return sumF;
+        }
+    }
+}
